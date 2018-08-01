51b24b79a984c4e9d19392eede3ae7eae1495fde
pHubertStasiuk <p.hubert.stasiuk@gmail.com>
2018-08-01 19:47:31 +0200

    Main.java Class modified

--- src/main/java/com/creditsuisse/assignment/Main.java
+++ src/main/java/com/creditsuisse/assignment/Main.java
@@ -1,8 +1,28 @@
 package com.creditsuisse.assignment;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    //for testing purposes assigned fixed file path
+    public static final String FILE_PATH = "C:\\Users\\Hubert\\Desktop\\log.json";
 
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //String filePath = JsonFileLog.inputTheFilePath();
+        JSONFileRetriever myJsonFile = new JSONFileRetriever(FILE_PATH);
+        if (!myJsonFile.isJSONFileExists() || !myJsonFile.isJSONFile()) {
+            throw new FileNotFoundException("The JSON file has not been found in the following path: "
+                    + FILE_PATH);
+        }
+        File myFile = myJsonFile.getFile();
+        LogsCollectionBuilder listBuilder = new LogsCollectionBuilder();
+        List<ServerLog> arr = listBuilder.parseFile(myFile);
+
+        for (int i = 0; i < arr.size()-1 ; i++) {
+            System.out.println(arr.get(i).getId());
+        }
     }
 }
